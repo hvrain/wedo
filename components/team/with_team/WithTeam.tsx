4e2a9a4646775231621cc5eddb4d81d5bac47e0d
leehj322 <gudwns8956@naver.com>
2024-10-28 19:43:32 +0900

    refactor: 각 컴포넌트마다 여러번 호출되던 useQuery 수정
    - 상위 컴포넌트에서 한 번만 실행하도록 수정

--- components/team/with_team/WithTeam.tsx
+++ components/team/with_team/WithTeam.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Container from "@/components/@common/container/Container";
+import { useGetTeam } from "@/queries/group";
 
 import TeamMemberList from "./TeamMemberList";
 import TeamReport from "./TeamReport";
@@ -11,16 +14,18 @@ interface WithTeamProps {
 }
 
 export default function WithTeam({ isAdmin, groupId }: WithTeamProps) {
+  const { data } = useGetTeam(groupId);
+
   return (
     <Container background="white">
       <div className="pb-6 pt-6 pc:pb-8 pc:pt-8">
-        <TeamTitle isAdmin={isAdmin} groupId={groupId} />
+        <TeamTitle isAdmin={isAdmin} groupId={groupId} teamData={data} />
         <div className="mb-12 mt-6 pc:mb-8 pc:mt-8">
-          <TeamTodoList isAdmin={isAdmin} groupId={groupId} />
+          <TeamTodoList isAdmin={isAdmin} groupId={groupId} teamData={data} />
         </div>
-        {isAdmin && <TeamReport groupId={groupId} />}
+        {isAdmin && <TeamReport teamData={data} />}
         <div className="mt-12 pc:mt-8">
-          <TeamMemberList isAdmin={isAdmin} groupId={groupId} />
+          <TeamMemberList isAdmin={isAdmin} groupId={groupId} teamData={data} />
         </div>
       </div>
     </Container>
