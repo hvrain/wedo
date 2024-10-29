874d7d912be9372d08dcb7664609a34dd8a48a10
gjrefa9139 <gjrefa9139@gmail.com>
2024-10-29 18:43:36 +0900

    Feat: 게시글/댓글 수정 기능 구현

--- app/(pages)/(자유게시판)/board/[id]/action.ts
+++ app/(pages)/(자유게시판)/board/[id]/action.ts
@@ -3,21 +3,47 @@
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
-import { deleteArticle } from "@/apis/article";
-import { deleteComment } from "@/apis/articleComment";
+import {
+  patchArticle,
+  patchArticleComment,
+  deleteArticle,
+  deleteArticleComment,
+} from "@/apis/article";
+import T from "@/dtos/Article";
 
-export async function handleDeleteArticle(articleId: number) {
+export async function actionPatchArticle(
+  articleId: number,
+  param: T.ArticleContent,
+) {
+  await patchArticle(articleId, param);
+
+  revalidatePath("/", "layout");
+}
+
+export async function actionPatchArticleComment(
+  id: {
+    articleId: number;
+    commentId: number;
+  },
+  param: T.ArticleContent,
+) {
+  await patchArticleComment(id.commentId, param);
+
+  revalidatePath(`/board/${id.articleId}`);
+}
+
+export async function actionDeleteArticle(articleId: number) {
   await deleteArticle(articleId);
 
   revalidatePath("/", "layout");
   redirect("/boards");
 }
 
-export async function handleDeleteComment(id: {
+export async function actionDeleteArticleComment(id: {
   articleId: number;
   commentId: number | null;
 }) {
-  await deleteComment(id.commentId!);
+  await deleteArticleComment(id.commentId!);
 
   revalidatePath(`/board/${id.articleId}`);
 }
