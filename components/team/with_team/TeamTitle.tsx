a77e68980e25cb54678fc2b8d04ffb6a94ce89dd
leehj322 <gudwns8956@naver.com>
2024-10-31 03:34:16 +0900

    refactor: 팀 수정하기 모달 리팩토링 #98
    - 새로운 모달 컴포넌트 적용
    - react-hook-form 사용
    - 추가적인 에러 처리
    - image input 추가

--- components/team/with_team/TeamTitle.tsx
+++ components/team/with_team/TeamTitle.tsx
@@ -1,19 +1,12 @@
 "use client";
 
-import { ChangeEvent, MouseEvent, useState } from "react";
-
-import { useQueryClient } from "@tanstack/react-query";
-
-import { Button } from "@/@common/Button";
-import Input from "@/@common/Input";
 import ActionsDropDown from "@/@common/dropdown/ActionsDropDown";
-import Modal from "@/@common/modal/Modal";
 import { GetTeamResponse } from "@/dtos/GroupDtos";
 import useToggle from "@/hooks/useToggle";
 import Sawtooth from "@/public/svg/sawtooth.svg";
-import { useEditTeam } from "@/queries/group";
 
 import TeamDelModal from "./TeamDelModal";
+import TeamEditModal from "./TeamEditModal";
 
 interface TeamTitleProps {
   isAdmin: boolean;
@@ -26,52 +19,11 @@ export default function TeamTitle({
   groupId,
   teamData,
 }: TeamTitleProps) {
-  const queryClient = useQueryClient();
-
   const [isEditModalOpen, toggleIsEditModalOpen] = useToggle(false);
   const [isDelModalOpen, toggleIsDelModalOpen] = useToggle(false);
 
-  // string | undefined로 추론되기 때문에 우선은 as string으로 처리하였으나 error처리 이후에 수정 필요
-  const imageUrl = teamData?.image as string;
-  const currentTeamName = teamData?.name as string;
-
-  const [teamNameInputValue, setTeamNameInputValue] = useState(currentTeamName);
-  const [teamNameInputErrorMessage, setTeamNameInputErrorMessage] =
-    useState("");
-
-  const { mutate: editTeam } = useEditTeam();
-
-  // 팀수정하기 모달의 input change 핸들러
-  const handleTeamNameInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setTeamNameInputValue(e.target.value);
-
-    if (teamNameInputErrorMessage) {
-      setTeamNameInputErrorMessage("");
-    }
-  };
-
-  // 팀수정하기 모달의 수정하기 button click 핸들러
-  // 1. 팀 이름이 이전과 동일하면 api 요청 x + error message
-  // 2. 이외에는 수정하기 요청을 보내고 error message를 띄움 성공시에는 팀 페이지의 이름 변경을 위해 invalidateQuery
-  const handleTeamEditButtonClick = (e: MouseEvent) => {
-    if (currentTeamName === teamNameInputValue) {
-      e.stopPropagation();
-      setTeamNameInputErrorMessage("팀 이름이 동일합니다.");
-      return;
-    }
-
-    editTeam(
-      { groupId, image: imageUrl, name: teamNameInputValue },
-      {
-        onError: (error) => {
-          e.stopPropagation();
-          setTeamNameInputErrorMessage(error.message);
-        },
-        onSuccess: () =>
-          queryClient.invalidateQueries({ queryKey: ["team", groupId] }),
-      },
-    );
-  };
+  const imageUrl = teamData?.image || "";
+  const currentTeamName = teamData?.name || "";
 
   return (
     <div className="xl-bold flex h-[64px] justify-between rounded-xl bg-dropDown-default px-6 py-5 text-default-light">
@@ -85,28 +37,13 @@ export default function TeamTitle({
             <Sawtooth />
           </ActionsDropDown>
 
-          {/* 팀 수정하기 모달 */}
-          <Modal
-            trigger={isEditModalOpen}
-            type="modal"
-            title="팀 이름"
-            hasCrossCloseIcon
-            onOpenChange={toggleIsEditModalOpen}
-            footer={
-              <Button className="flex-1" onClick={handleTeamEditButtonClick}>
-                수정 하기
-              </Button>
-            }
-          >
-            <Input
-              placeholder="팀 이름을 작성해주세요."
-              onChange={handleTeamNameInputChange}
-              defaultValue={currentTeamName}
-              value={teamNameInputValue}
-              Error={!!teamNameInputErrorMessage}
-              ErrorMessage={teamNameInputErrorMessage}
-            />
-          </Modal>
+          <TeamEditModal
+            isOpen={isEditModalOpen}
+            toggleIsOpen={toggleIsEditModalOpen}
+            groupId={groupId}
+            currentTeamImageUrl={imageUrl}
+            currentTeamName={currentTeamName}
+          />
 
           <TeamDelModal
             isOpen={isDelModalOpen}
